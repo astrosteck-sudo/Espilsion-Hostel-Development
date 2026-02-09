fd4c02a113f968e54d5fa160a79804edfdac9f94
astrosteck-sudo <astrosteck@gmail.com>
2026-02-08 19:10:41 -0800

    Add files via upload

---
+++ logins/sign_up.js
@@ -0,0 +1,49 @@
+import { hostels } from "../hostel-data/hostel_data.js";
+
+
+
+//THIS IS FOR THE HAMBURGER BUTTON
+const hamburger = document.querySelector('.hamburger-button');
+const navLinks = document.querySelector('.navigation-links');
+
+//THIS TOGGLES THE HAMBURGER BUTTON ON AND OFF
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
+
+
+//THIS DISPLAYS THE CURRENT IMAGE, THEN INCREASES THE INDEX BY ONE,
+//  THEN CHECKS IF THE INDEX IS GREATOR THAN THE LENGTH OF THE ARRAY 
+// OF IMGES IF SO IT SETS THE INDEX BACK TO 0 AND STARTS AGAIN
+let index = 0;
+setInterval(() => {
+  document.body.style.backgroundImage = `url(${hostels[index].image})`;
+  index++;
+  if (index >= hostels.length) {
+    index = 0; // loop back to the first image
+  }
+}, 6000); // change every 6 seconds
+
+
+//THIS TOGGLES THE EYE BETWEEN OPEN AND CLOSE
+const eyeImage = document.querySelector('#toggle-eye');
+const input = document.querySelector('#user-password');
+eyeImage.addEventListener('click', () => {
+    if(input.type === 'password'){
+        eyeImage.src = '../icons/visible.png';
+        input.type = 'text';
+    }else{
+        input.type = 'password';
+        eyeImage.src = '../icons/closed-eyes.png';
+        // eyeToggle = 'closed';
+    }
+    
+})
+
+
+
+
+
+
+
+
