01c6e1fd0b3a5fbf96ae4dca8de861c1122ac9c7
astrosteck-sudo <astrosteck@gmail.com>
2026-02-16 17:53:09 -0800

    Add files via upload

---
+++ more_from_us/projects.js
@@ -0,0 +1,28 @@
+export const projects = [
+    {
+        Name: 'FlexPay 26',
+        Role: 'This Program is tailored at providing quick and affordable Gold and Diamonds to Free Fire gamers.In further time , it is planed to be scaled to accomodate other games like PurbG, Delta Force, Call Of Duty, And Fortnite.',
+        StatusComplete: false,
+        Link: '#',
+    },{
+        Name: 'FlexPay 26',
+        Role: 'This Program is tailored at providing quick and affordable Gold and Diamonds to Free Fire gamers.In further time , it is planed to be scaled to accomodate other games like PurbG, Delta Force, Call Of Duty, And Fortnite.',
+        StatusComplete: false,
+        Link: '#',
+    },{
+        Name: 'FlexPay 26',
+        Role: 'This Program is tailored at providing quick and affordable Gold and Diamonds to Free Fire gamers.In further time , it is planed to be scaled to accomodate other games like PurbG, Delta Force, Call Of Duty, And Fortnite.',
+        StatusComplete: false,
+        Link: '#',
+    },{
+        Name: 'FlexPay 26',
+        Role: 'This Program is tailored at providing quick and affordable Gold and Diamonds to Free Fire gamers.In further time , it is planed to be scaled to accomodate other games like PurbG, Delta Force, Call Of Duty, And Fortnite.',
+        StatusComplete: false,
+        Link: '#',
+    },{
+        Name: 'FlexPay 26',
+        Role: 'This Program is tailored at providing quick and affordable Gold and Diamonds to Free Fire gamers.In further time , it is planed to be scaled to accomodate other games like PurbG, Delta Force, Call Of Duty, And Fortnite.',
+        StatusComplete: false,
+        Link: '#',
+    },
+]
